0fdc8ad923ffb9c86732b1a7c3772174b80d94d3
Juan Camacho Mohedano <jucamohedano@gmail.com>
2024-05-02 17:22:41 +0200

    add cuda

--- labs/train_bert.py
+++ labs/train_bert.py
@@ -1,6 +1,6 @@
 # Global variables
 import os
-device = "cpu" #means we are using the GPU with id 0, if you have multiple GPU
+device = "cuda:0" #means we are using the GPU with id 0, if you have multiple GPU
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1" # Used to report errors on CUDA side
 PAD_TOKEN = 0
 PRINT_DATA_STATS=0
